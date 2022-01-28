42b8d9993c174f79225f02fa750faf3263f2575c
王禕淼 <wangyimiao@ciicsh.com>
2022-01-28 10:01:55 +0800

    footer固定位置

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import RouterApp from './router';
 import Layout from '@/components/Layout/Layout'
-// import './index.css';
-// import App from './App';
-// import TestFetch from './components/UseApiDemo2';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   // 严格模式，用于提示各类错误
   <React.StrictMode> 
-    {/* <App /> */}
-    {/* <TestFetch />*/}
     <Layout></Layout>
   </React.StrictMode>
   ,
@@ -21,6 +15,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
 
 
