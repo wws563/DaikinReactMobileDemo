f2921cb846ca487ad8929144409190926bcd3b0a
王禕淼 <wangyimiao@ciicsh.com>
2022-01-30 10:07:37 +0800

    配置全局less

--- README.md
+++ README.md
@@ -8,6 +8,10 @@
 1. `npm i less less-loader`
 2. 在eject暴露配置文件中增加less相关设置[参考](https://www.cnblogs.com/shun1015/p/13520577.html)
 3. 报错，查询后发现webpack版本为4.x,less-loader直接安装为10.x版。版本过高，降级5.0后正常使用。
+
 ### 安装mockjs模拟数据
+
+#### 踩坑记录
+1. react项目开启了`css module`会把`class`编辑成哈希值，全局变量需要使用`:global`转换一下。
 
 to be continued...
--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from '@/components/Layout/Layout'
 import reportWebVitals from './reportWebVitals';
-// import '@content/css/common.less';
+import './content/css/common.less';
 import './index.css';
 
 ReactDOM.render(
   // 严格模式，用于提示各类错误
   <React.StrictMode> 
     <Layout>
-      <div className='alert'>test show position</div>
+
     </Layout>
+    <div className='alert'>test show position</div>
   </React.StrictMode>
   ,
   document.getElementById('root')
