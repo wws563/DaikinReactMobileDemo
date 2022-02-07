6a14da92448c23daaf0ac65f2413d4ebc811efe6
王禕淼 <wangyimiao@ciicsh.com>
2022-02-07 19:13:56 +0800

    更新mock模拟接口

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,28 +1,38 @@
 
 import Mock from 'mockjs'
+
 import styles from '@/content/css/home.less'
+import '@/scripts/mock'
 
-
+// 数据部分
 const listData = Mock.mock({"testList|10":[
 	{id:'@guid',name:'@ctitle'}]}).testList
-console.log(listData)
 
-const listItems=listData.map((item,index)=>{
+
+const listItems = listData.map((item,index)=>{
 	return(
-		<div>
+		<div key={item.id}>
 			<span>{item.id}</span>
 			<span>{item.name}</span>
 		</div>
 	)
 })
 
-function Home(){
+// const getList = async()=>{
+// 	res = await Api.getList
+// }
+
+function List(){
 	return (
 	<div className={styles.container}>
+		{/* 搜索栏 */}
+		<div></div>
+		{/* 下拉搜索 */}
+		{/* 列表内容 */}
 		{listItems}
 	</div>
 
 	)
 }
 
-export default Home
+export default List
